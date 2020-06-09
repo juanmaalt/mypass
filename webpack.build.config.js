1d7260e96d274da781607a0c91f77179ce532981
juanmaalt <juanmaalt@gmail.com>
2020-06-09 01:25:43 -0300

    Updating configuration

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,8 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BabiliPlugin = require("babili-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 // Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
 const defaultInclude = path.resolve(__dirname, "src");
@@ -15,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
         include: defaultInclude,
       },
       {
@@ -40,17 +39,14 @@ module.exports = {
   target: "electron-renderer",
   plugins: [
     new HtmlWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "bundle.css",
-      chunkFilename: "[id].css",
-    }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production"),
     }),
-    new BabiliPlugin(),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   stats: {
     colors: true,
     children: false,
